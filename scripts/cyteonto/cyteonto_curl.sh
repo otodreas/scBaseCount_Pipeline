09101f2117017b40f09ac4ac3e735bc815a59cd3
Oliver Todreas <otodreas@gmail.com>
2026-04-24 12:43:19 +0200

    organize outputs

--- scripts/cyteonto/cyteonto_curl.sh
+++ scripts/cyteonto/cyteonto_curl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl -sS -X POST "https://cyteonto.nygen.io/compare" \
   -H 'Content-Type: application/json' \
-  -d @"$(dirname "$0")/../../tmp/annotations.json" > "$(dirname "$0")/../../tmp/cyteonto_response.json"
+  -d @"$(dirname "$0")/../../tmp/annotations.json" > "$(dirname "$0")/../../output/cyteonto/response/cyteonto_response.json"
