6febaf1b837e6826d72f2105efed4db2875a3897
Oliver Todreas <otodreas@gmail.com>
2026-07-06 14:13:20 +0200

    remove celltypist package

--- scripts/celltypist_runner/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from celltypist_runner.config import CellTypistRunnerConfig
-from celltypist_runner.runner import annotate_celltypist
-
-__all__ = [
-    "CellTypistRunnerConfig",
-    "annotate_celltypist",
-]
