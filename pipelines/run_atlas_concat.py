8defa67099422c09dbb6119ddbe3aca3525c7d14
Oliver Todreas <otodreas@gmail.com>
2026-07-16 14:42:11 +0200

    update todo

--- pipelines/run_atlas_concat.py
+++ pipelines/run_atlas_concat.py
@@ -1,6 +1,5 @@
 from h5ad_concat import H5adConcatConfig, run_h5ad_concat
 
-# TODO: confirm contexts on server match local
 cfg = H5adConcatConfig(
     atlasR2Key="lung/atlas_v1.h5ad",
     uploadAtlas=True,
