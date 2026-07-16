26261995024fca71f656e6e822d99cd05eba7773
Oliver Todreas <otodreas@gmail.com>
2026-07-16 14:38:25 +0200

    fix default

--- pipelines/run_atlas_concat.py
+++ pipelines/run_atlas_concat.py
@@ -3,6 +3,7 @@ from h5ad_concat import H5adConcatConfig, run_h5ad_concat
 # TODO: confirm contexts on server match local
 cfg = H5adConcatConfig(
     atlasR2Key="lung/atlas_v1.h5ad",
+    uploadAtlas=True,
 )
 
 run_h5ad_concat(cfg)
