036411305f9eaa27496aa64a37b2cfcd1feda748
Oliver Todreas <otodreas@gmail.com>
2026-07-16 16:19:44 +0200

    new test data, import env vars

--- pipelines/run_atlas_concat.py
+++ pipelines/run_atlas_concat.py
@@ -1,8 +1,14 @@
+from dotenv import load_dotenv
 from h5ad_concat import H5adConcatConfig, run_h5ad_concat
+from shared.repo import REPO_ROOT
+
+load_dotenv(REPO_ROOT / ".env")
 
 cfg = H5adConcatConfig(
-    atlasR2Key="lung/atlas_v1.h5ad",
-    uploadAtlas=True,
+    datasetsPath=REPO_ROOT / "tests/datasets_sample20.csv",
+    # atlasR2Key="lung/atlas_v1.h5ad",
+    uploadAtlas=False,
+    outputPath=REPO_ROOT / "output" / "atlas" / "data" / "atlas_sample20.h5ad",
 )
 
 run_h5ad_concat(cfg)
