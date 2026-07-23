6b283c8e39ea30e301ecd8fee0ad69d6698b24ca
Oliver Todreas <otodreas@gmail.com>
2026-07-23 11:44:53 +0200

    add standard pdb cfg, ignore all atlases, fix sample atlas pipe

--- pipelines/sample_atlas.py
+++ pipelines/sample_atlas.py
@@ -2,11 +2,15 @@ from pathlib import Path
 
 import scanpy as sc
 
-FULL_ADATA_PATH: Path = Path("output/atlas/v1/processed1/atlas_harmony.h5ad")
-SAMPLE_ADATA_PATH: Path = Path("output/atlas/v1/processed1/atlas_harmony_sub_100k.h5ad")
+FULL_ADATA_PATH: Path = Path(__file__).resolve().parents[1] / "output/atlas/v1/processed_1/atlas_harmony.h5ad"
+SAMPLE_ADATA_PATH: Path = (
+    Path(__file__).resolve().parents[1] / "output/atlas/v1/processed_1/atlas_harmony_sub_100k.h5ad"
+)
 N_OBS_SAMPLE: int = 100_000
 
 
 adata = sc.read_h5ad(FULL_ADATA_PATH, backed="r")
+print(f"Loaded full adata: {adata.n_obs} observations")
 sub = sc.pp.sample(adata, n=N_OBS_SAMPLE, copy=True)
+print(f"Subsampled adata: {sub.n_obs} observations, saving to {SAMPLE_ADATA_PATH}")
 sub.write_h5ad(SAMPLE_ADATA_PATH, compression="gzip")
