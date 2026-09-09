14c0f93f554070403bb4df7f0af1130e664a49d9
Oliver Todreas <otodreas@gmail.com>
2026-09-09 11:00:16 +0200

    update file paths

--- pipelines/run_atlas_concat.py
+++ pipelines/run_atlas_concat.py
@@ -9,11 +9,11 @@ load_dotenv(REPO_ROOT / ".env")
 # There, you will find qc settings.
 cfg = H5adConcatConfig(
     datasetsPath=REPO_ROOT / "output/metadata/datasets_v2.csv",
-    atlasR2Key="atlas/2026-08-12/atlas.h5ad",
+    atlasR2Key="atlas/2026-09-07/atlas.h5ad",
     uploadAtlas=True,
     minPctCellsAfterQc=0.5,
     maxPctRibo=0.5,
-    outputPath=REPO_ROOT / "output/atlas/2026-08-12/atlas.h5ad",
+    outputPath=REPO_ROOT / "output/atlas/2026-09-07/atlas.h5ad",  # Replace with your desired output path
 )
 
 print("Atlas concat pipeline started")
